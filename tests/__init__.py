1bb75fe13c01672c96793a483a2be162cfea2a33
Wen Kokke <wenkokke@users.noreply.github.com>
2022-10-02 13:45:45 +0100

    Actually test with 3.11

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,42 @@
+import inspect
+import re
+import sys
 import typing
+
+
+def pyver() -> str:
+    return f"py{sys.version_info.major}{sys.version_info.minor}"
+
+
+def short(sig: inspect.Signature) -> str:
+    out = str(sig)
+    out = re.sub(r"ForwardRef\('([^']+)'\)", r"\1", out)
+    out = out.replace("tree_sitter_type_provider.node_types.", "")
+    out = out.replace("abc.Talon", "Talon")
+    out = re.sub(r"'Talon([^']+)'", r"Talon\1", out)
+    return out
+
+
+def function_signatures(object: object) -> typing.Iterator[str]:
+    for name, fun in inspect.getmembers(object, inspect.isfunction):
+        if not (name.startswith("_") or name in ["to_dict", "to_json"]):
+            try:
+                funsig = inspect.signature(fun)
+                yield f"{name}{short(funsig)}"
+            except ValueError:
+                pass
+
+
+def class_signatures(object: object) -> typing.Iterator[str]:
+    for name, cls in inspect.getmembers(object, inspect.isclass):
+        if not name.startswith("_"):
+            try:
+                clssig = inspect.signature(cls)
+                yield f"{name}{short(clssig)}"
+                for funsigstr in function_signatures(cls):
+                    yield f"  {funsigstr}"
+            except ValueError:
+                pass
 
 
 def node_dict_simplify(node_dict: typing.Dict[str, typing.Any]) -> None:
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,48 +1,13 @@
-import inspect
 import pathlib
-import re
 import typing
 
 import pytest
 
 import tree_sitter_type_provider
+from tests import class_signatures, function_signatures, pyver
 
 
-def short(sig: inspect.Signature) -> str:
-    out = str(sig)
-    out = out.replace("tree_sitter_type_provider.node_types.", "")
-    out = re.sub(r"ForwardRef\('(\w+)'\)", r"\1", out)
-    out = out.replace("typing.", "")
-    out = re.sub(r"Optional\[([^\]]+)\]", r"Union[\1, None]", out)
-    out = out.replace("NoneType", "None")
-    out = out.replace("abc.", "")
-    out = out.replace("~Result", "Result")
-    return out
-
-
-def function_signatures(object: object) -> typing.Iterator[str]:
-    for name, fun in inspect.getmembers(object, inspect.isfunction):
-        if not (name.startswith("_") or name in ["to_dict", "to_json"]):
-            try:
-                funsig = inspect.signature(fun)
-                yield f"{name}{short(funsig)}"
-            except ValueError:
-                pass
-
-
-def class_signatures(object: object) -> typing.Iterator[str]:
-    for name, cls in inspect.getmembers(object, inspect.isclass):
-        if not name.startswith("_"):
-            try:
-                clssig = inspect.signature(cls)
-                yield f"{name}{short(clssig)}"
-                for funsigstr in function_signatures(cls):
-                    yield f"  {funsigstr}"
-            except ValueError:
-                pass
-
-
-@pytest.mark.golden_test("data/golden/api/*.yml")
+@pytest.mark.golden_test(f"data/golden/api/*.{pyver()}.yml")
 def test_talon(golden):
     class_prefix = golden["input"]["class_prefix"]
     module_name = f"tree_sitter_{ golden['input']['name'] }"
